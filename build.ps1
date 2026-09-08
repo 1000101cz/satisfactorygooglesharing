1582e25da20c2d5e313c001daa1e9a5ee9364d52
Stepan Marousek <maroustepan@protonmail.ch>
2026-09-08 10:20:49 +0200

    feature: --include-windows-runtime-dlls=no

--- build.ps1
+++ build.ps1
@@ -3,6 +3,6 @@ pip install zstandard
 python -m nuitka --standalone --onefile --onefile-as-archive `
                 --plugin-enable=pyqt6 --noinclude-qt-translations `
                 --windows-console-mode=disable --include-data-dir=data=data `
-                --windows-icon-from-ico=data/icon.ico `
+                --windows-icon-from-ico=data/icon.ico --include-windows-runtime-dlls=no `
                 --nofollow-import-to=PyQt6.QtWebEngine,PyQt6.QtWebEngineCore,PyQt6.QtQuick,PyQt6.QtQml,PyQt6.Qt3D,PyQt6.QtSql,PyQt6.QtTest,PyQt6.QtSensors `
                 --output-filename=SatisfactorySaveSync.exe main.py
