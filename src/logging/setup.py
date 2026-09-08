29d65155e5abdb1514e82765576babd635414dc1
Stepan Marousek <maroustepan@protonmail.ch>
2026-09-08 09:54:13 +0200

    fix: console logging only if console is available

--- src/logging/setup.py
+++ src/logging/setup.py
@@ -6,11 +6,13 @@ from pathlib import Path
 
 def setup_logging():
     logger.remove()
-    logger.add(
-        sys.stderr, 
-        level="DEBUG", 
-        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-    )
+    
+    if sys.stderr is not None:
+        logger.add(
+            sys.stderr, 
+            level="DEBUG", 
+            format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+        )
 
     app_data = Path(os.environ.get("LOCALAPPDATA", Path.home()))
     log_dir = app_data / "SatisfactoryGoogleSharing" / "logs"
